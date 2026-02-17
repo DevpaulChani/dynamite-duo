c6c0eb7c1c9a5212a99c5b142343ec8c5370c7d2
DevpaulChani <devpaulc@gmail.com>
2026-02-17 10:06:53 -0600

    target dom

---
+++ scripts/database.js
@@ -0,0 +1,40 @@
+export const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ],
+    villans: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+                {
+            id: 3,
+            name: "DrDoom",
+            power: "Magic"
+        },
+        {
+            id: 4,
+            name: "Loki",
+            power: "Magic"
+        }
+    ]
+}
+
+export const getVillains = () => {
+    return structuredClone(database.villains);
+}
